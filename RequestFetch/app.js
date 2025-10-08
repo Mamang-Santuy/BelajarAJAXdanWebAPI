807ac014a5a7b63b5b78f98713fbeedf58dfbab4
unknown <sayidrofi2951@gmail.com>
2025-10-08 20:38:26 +0700

    praktek 2

---
+++ RequestFetch/app.js
@@ -0,0 +1,59 @@
+// const req = new XMLHttpRequest()
+// let data;
+
+// req.onload = function (){
+//     data = JSON.parse(this.responseText)
+//     console.log(data)
+// }
+
+// req.onerror = function(){
+//     console.log('Error', this)
+// }
+
+// req.open('GET','https://swapi.dev/api/people/1', true)
+// myReq.setRequestHeader('Accept', 'application/json')
+// req.send()
+
+
+//REQUEST FETCH
+
+// fetch('https://swapi.dev/api/people/1')
+//     .then((res) => {
+//         if(!res.ok){
+//             throw Error('tidak dapat melakukan fetch')
+//         }
+//         return res.json()
+//     })
+//     .then((data)=>{
+//         console.log('json 1',data)
+//         return fetch('https://swapi.dev/api/people/2')
+//     })
+//     .then((res)=>{
+//         console.log('request 2')
+//         if(!res.ok){
+//             throw Error('tidak dapat melakukan fetch')
+//         }
+//         return res.json()
+//     })
+//     .then((data)=>{
+//         console.log(data)
+//     })
+//     .catch((err)=>{
+//         console.log('error',err)
+//     })
+
+const loadPeople = async()=>{
+    try{
+        const res = await fetch('https://swapi.dev/api/people/1')
+    const data = await res.json()
+    console.log(data)
+    const res2 = await fetch('https://swapi.dev/api/people/1')
+    const data2 = await res2.json()
+    console.log(data2)
+    } catch(err){
+        console.log('Error', err)
+    }
+    
+}
+
+loadPeople()
